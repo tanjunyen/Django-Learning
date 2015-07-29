94905699dbba53658b37c1cf29dd280815aaeed0
tanjunyen <tanjunyen@gmail.com>
2015-07-29 12:59:15 +0200

    User log in
    Manually coded user log in, authentication, logged in session, invalid
    log in handling and log out. Added related templates which extends a
    base template.

--- taskrabbit/main/urls.py
+++ taskrabbit/main/urls.py
@@ -5,5 +5,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+
+    url(r'accounts/login/$', views.log_in),
+    url(r'accounts/auth/$', views.auth_view),
+    url(r'accounts/logout/$', views.log_out),
+    url(r'accounts/loggedin/$', views.logged_in),
+    url(r'accounts/invalid/$', views.invalid_login),
 ]
 
--- taskrabbit/main/views.py
+++ taskrabbit/main/views.py
@@ -1,10 +1,38 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template.loader import get_template
 from django.template import Context
+from django.contrib.auth import authenticate, login, logout
+from django.core.context_processors import csrf
 # Create your views here.
 
 def index(request):
     t = get_template('base.html')
     html = t.render(Context({'name': 'JunYen'}))
     return HttpResponse(html)
+
+def log_in(request):
+    c = {}
+    c.update(csrf(request))
+    return render_to_response('login.html', c)
+
+def auth_view(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect('/accounts/loggedin')
+    else:
+            return HttpResponseRedirect('/accounts/invalid')
+
+def logged_in(request):
+    return render_to_response('loggedin.html', { 'full_name' : request.user.username })
+
+def invalid_login(request):
+    return render_to_response('invalid.login.html')
+
+def log_out(request):
+    logout(request)
+    return render_to_response('logout.html')
